2e8d130eb04a2fa594d96d88be28db596ddb49c3
Anamay-Upreti <rcupretidgc13@gmail.com>
2024-06-10 23:02:34 +0530

    started-adding-redux

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -4,18 +4,28 @@ import { copy, linkIcon, loader, tick } from "../assets"
 import { CgEnter } from "react-icons/cg"
 import { ImEnter } from "react-icons/im"
 const Demo = () => {
+  const [aritcle, setAritcle] = useState({
+    url: '',
+    summary: ''
+});
+
+const handleSubmit = async (e) =>{
+  alert('Submitted')
+}
   return (
     <section className="mt-16 w-full max-w-xl">
       <div className="flex flex-col w-full gap-2">
         <form className="justify-center relative flex w-full items-center"
-        onSubmit={() =>{}}>
+        onSubmit={handleSubmit}>
           <img src={linkIcon} alt="link_icon"
           className="abosulte left-0 my-2 ml-3 w-5" />
 
           <input type="url" 
           placeholder="Enter A URL"
-          value=""
-          onChange={()=>{}}
+          value={aritcle.url}
+          onChange={(e)=>setAritcle({
+            ...aritcle, url: e.target.value
+          })}
           required
           className="url_input peer"
           />
@@ -26,7 +36,12 @@ const Demo = () => {
             <ImEnter/>
           </button>
         </form>
+        {/* browse URL history */}
+
       </div>
+
+      {/* display results */}
+
     </section>
   )
 }
